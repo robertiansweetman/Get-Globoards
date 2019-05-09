fff6537f3ef9b79b89b9f8c7c43f64fdc58a89f4
Robert Sweetman <robertiansweetman@gmail.com>
2019-05-09 12:38:41 +0100

    Add comments to cards

--- Add-Comment.ps1
+++ Add-Comment.ps1
@@ -1,8 +1,28 @@
-function add-Comment {
+function Add-Comment {
 
 param(
     [Parameter(Mandatory=$true)]
     [string]$comment
 )
+
+    # need to replace this with something smarter
+    . ($PSScriptRoot + "./secrets.ps1")
+
+    $boardId = Get-Board
+
+    $cardId = Get-CardId -boardId $boardId
+    
+$bodyJson=@"
+{
+"text": "$comment"
+}
+"@
+
+$newToken = ConvertTo-SecureString -String $token -AsPlainText -Force
+
+$link = "https://$url/boards/"+ $boardId + "/cards/" + $cardId + "/comments"
+
+# TODO: Consider better error handling at this step also...
+Invoke-RestMethod -Method Post -Body $bodyJson -Uri $link -ContentType 'application/json' -Token $newToken -Authentication Bearer
 
 }
---
+++ Get-CardId.ps1
@@ -0,0 +1,33 @@
+function Get-CardId {
+
+    param(
+        $boardId
+    )
+
+    . ($PSScriptRoot + "./secrets.ps1")
+
+    $newToken = ConvertTo-SecureString -String $token -AsPlainText -Force
+
+    $uri = "https://$url/boards/"+ $boardId + "/cards"
+
+    $cards = Invoke-RestMethod -Uri $uri -Method GET -ContentType 'application/json' -Token $newToken -Authentication Bearer
+
+    $listCards = @()
+    
+    foreach ($item in $cards) {
+        $listCards += $item
+        Write-Host ($listCards).name
+    }
+
+    for ($i = 0; $i -lt $listCards.length; $i++) {
+        Write-Host "To update"($listCards[$i]).name"select:" ($1++ + 1)
+    }
+
+    Do { $cardChoice = Read-Host -Prompt "Select which card to add comment to"} 
+    while ((1..$listCards.length) -notcontains $cardChoice)
+    
+    $cardId = ($listCards[$cardChoice - 1]).id    
+    
+    return $cardId
+
+}
